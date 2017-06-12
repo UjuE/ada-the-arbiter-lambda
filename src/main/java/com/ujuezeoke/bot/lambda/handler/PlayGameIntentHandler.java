cdd6586e2e8c411ba6ba164d4b08d23ebc35e5ae
Obianuju Vivian Ezeoke <ujuezeoke@gmail.com>
2017-06-12 20:18:02 +0100

    More Hacked in stuff

--- src/main/java/com/ujuezeoke/bot/lambda/handler/PlayGameIntentHandler.java
+++ src/main/java/com/ujuezeoke/bot/lambda/handler/PlayGameIntentHandler.java
@@ -3,6 +3,7 @@ package com.ujuezeoke.bot.lambda.handler;
 import com.ujuezeoke.bot.template.model.request.LexBotRequest;
 import com.ujuezeoke.bot.template.model.response.LexBotResponse;
 import com.ujuezeoke.bot.template.model.response.LexBotResponseBuilder;
+import com.ujuezeoke.bot.template.model.response.Slot;
 import com.ujuezeoke.bot.template.model.response.model.dialogaction.FulfillmentState;
 import com.ujuezeoke.game.TwoPlayerGame;
 import com.ujuezeoke.game.gameplay.playerdetails.PlayerDetails;
@@ -10,8 +11,10 @@ import com.ujuezeoke.game.gameplay.playerdetails.PlayerLabel;
 import com.ujuezeoke.game.gameplay.result.GamePlayResult;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static com.ujuezeoke.bot.template.model.response.model.dialogaction.message.DialogActionMessageContentType.PlainText;
+import static java.util.stream.Collectors.joining;
 
 /**
  * Created by Obianuju Ezeoke on 11/06/2017.
@@ -30,7 +33,12 @@ public class PlayGameIntentHandler {
 
     public LexBotResponse process() {
         final Map<String, Object> slots = input.getCurrentIntent().getSlots();
-        if (slots.containsKey(PLAYER_ONE_LABEL) && slots.containsKey(PLAYER_TWO_LABEL)) {
+        if (slots.containsKey(PLAYER_ONE_LABEL)
+                && slots.containsKey(PLAYER_TWO_LABEL)
+                && slots.containsKey(GAME_NAME_SLOT_NAME)
+                && Optional.ofNullable(slots.get(PLAYER_ONE_LABEL)).isPresent()
+                && Optional.ofNullable(slots.get(PLAYER_TWO_LABEL)).isPresent()
+                && Optional.ofNullable(slots.get(GAME_NAME_SLOT_NAME)).isPresent()) {
             final TwoPlayerGame twoPlayerGame =
                     availableGamesMap.get(slots.get(GAME_NAME_SLOT_NAME).toString().replaceAll(" ", ""));
             final GamePlayResult gamePlayResult = twoPlayerGame.playComputerVsComputer(
@@ -42,11 +50,25 @@ public class PlayGameIntentHandler {
                     .withMessage(PlainText, messageFrom(gamePlayResult))
                     .withFulfilmentState(FulfillmentState.Fulfilled)
                     .build();
+        } else if (!Optional.ofNullable(slots.get(GAME_NAME_SLOT_NAME)).isPresent()) {
+            return new LexBotResponseBuilder()
+                    .buildElicitSlotDialogActionResponse()
+                    .withIntentName(input.getCurrentIntent().getName())
+                    .withSlot(new Slot(GAME_NAME_SLOT_NAME, slots.getOrDefault(GAME_NAME_SLOT_NAME, null)))
+                    .withSlot(new Slot(PLAYER_ONE_LABEL, slots.getOrDefault(PLAYER_ONE_LABEL, null)))
+                    .withSlot(new Slot(PLAYER_TWO_LABEL, slots.getOrDefault(PLAYER_TWO_LABEL, null)))
+                    .withSlotToElicit(GAME_NAME_SLOT_NAME)
+                    .withMessage(PlainText,
+                            "What game would you like to play? \n"
+                                    +availableGamesMap.keySet().stream().collect(joining(",\n")))
+                    .build();
         } else {
             return new LexBotResponseBuilder()
                     .buildElicitSlotDialogActionResponse()
                     .withIntentName(input.getCurrentIntent().getName())
-                    .withSlots(slots)
+                    .withSlot(new Slot(GAME_NAME_SLOT_NAME, slots.getOrDefault(GAME_NAME_SLOT_NAME, null)))
+                    .withSlot(new Slot(PLAYER_ONE_LABEL, slots.getOrDefault(PLAYER_ONE_LABEL, null)))
+                    .withSlot(new Slot(PLAYER_TWO_LABEL, slots.getOrDefault(PLAYER_TWO_LABEL, null)))
                     .withSlotToElicit(determine(slots))
                     .withMessage(PlainText, "who is " + determine(slots))
                     .build();
@@ -54,7 +76,7 @@ public class PlayGameIntentHandler {
     }
 
     private String determine(Map<String, Object> slots) {
-        if (slots.containsKey(PLAYER_ONE_LABEL) && !slots.get(PLAYER_ONE_LABEL).toString().isEmpty()) {
+        if (slots.containsKey(PLAYER_ONE_LABEL) && !Optional.ofNullable(slots.get(PLAYER_ONE_LABEL)).isPresent()) {
             return PLAYER_ONE_LABEL;
         }
 
